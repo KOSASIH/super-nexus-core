3a10b57e94ce110e16a3bfbedec1248c72e41fc4
KOSASIH <kosasihg88@gmail.com>
2024-11-03 11:25:59 +0700

    Create App.js

---
+++ src/dapps/wallet/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Wallet from './components/Wallet';
+import './styles.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            <Wallet />
+        </div>
+    );
+};
+
+export default App;
