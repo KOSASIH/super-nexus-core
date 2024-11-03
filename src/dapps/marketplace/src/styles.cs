9d2020c9c449bb3766cea08aa8ea8bafb251e83f
KOSASIH <kosasihg88@gmail.com>
2024-11-03 11:39:17 +0700

    Create styles.cs

---
+++ src/dapps/marketplace/src/styles.cs
@@ -0,0 +1,16 @@
+.App {
+    text-align: center;
+    margin: 20px;
+}
+
+input {
+    margin: 10px;
+    padding: 10px;
+    width: 200px;
+}
+
+button {
+    margin: 10px;
+    padding: 10px;
+    cursor: pointer;
+}
