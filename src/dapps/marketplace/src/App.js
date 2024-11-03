b1d8459c45f5950c5021ddddcc19cbb421d6f3f6
KOSASIH <kosasihg88@gmail.com>
2024-11-03 11:35:40 +0700

    Create App.js

---
+++ src/dapps/marketplace/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Marketplace from './components/Marketplace';
+import './styles.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            <Marketplace />
+        </div>
+    );
+};
+
+export default App;
